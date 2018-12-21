2838494c58fcb55dd7c818000cf11cac85f4116c
JaredPatterson6 <jaredmichaelpatterson@gmail.com>
2018-12-21 09:53:21 -0800

    Add files via upload
    A good starting place. The prototype XML Layout files are setup.

---
+++ PrototypeLayouts/app/src/main/java/com/example/a745715/prototypelayouts/Metronome.java
@@ -0,0 +1,13 @@
+package com.example.a745715.prototypelayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Metronome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metronome);
+    }
+}
---
+++ PrototypeLayouts/app/src/main/java/com/example/a745715/prototypelayouts/Tuner.java
@@ -0,0 +1,13 @@
+package com.example.a745715.prototypelayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tuner extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuner);
+    }
+}
